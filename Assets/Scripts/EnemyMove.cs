dd11f9a0cd097a4653d6d43befcb7b8fbbe43005
Babitk21 <77804801+Babitk21@users.noreply.github.com>
2021-02-16 11:38:10 +0530

    yo

---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    private Rigidbody2D EnemyRB;
+    public float delta = 1.5f;  // Amount to move left and right from the start point
+    public float speed = 2.0f;
+    private Vector3 startPos;
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnemyRB = GetComponent<Rigidbody2D>();
+        startPos = transform.position;
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetBool("EnemyMovement", true);
+        Vector3 v = startPos;
+        v.x += delta * Mathf.Sin(Time.time * speed);
+        transform.position = v;
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEditor.SceneManagement;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody2D PlayerRB;
+    public float speed;
+    private float jumpSpeed = 500;
+    private bool collisionChecker = true;
+    private Animator anim;
+    public TextMeshProUGUI points;
+    private int PointsCollected = 0;
+    public TextMeshProUGUI lifes;
+    static private int lifesRemaining = 3;
+    public TextMeshProUGUI GameOver;
+    public Button restartButton;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerRB = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            PlayerRB.AddForce(Vector2.left * speed);
+            anim.SetBool("IsRunningLeft", true);
+        }
+        else
+        {
+            anim.SetBool("IsRunningLeft", false);
+        }
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            PlayerRB.AddForce(Vector2.right * speed);
+            anim.SetBool("IsRunningRight", true);
+        }
+        else
+        {
+            anim.SetBool("IsRunningRight", false);
+        }
+        if(Input.GetKey(KeyCode.Space) && collisionChecker)
+        {
+            PlayerRB.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+            collisionChecker = false;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collisionChecker = true;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag=="Points")
+        {
+            Destroy(collision.gameObject);
+            PointsCollected++;
+            points.text = "score = " + PointsCollected;
+        }
+        else if(collision.gameObject.tag=="Enemy")
+        {
+            Destroy(gameObject);
+            lifesRemaining--;
+            lifes.text = "lifes = " + lifesRemaining;
+            restartButton.gameObject.SetActive(true);
+            GameOver.gameObject.SetActive(true);                      
+        }
+    }
+}
